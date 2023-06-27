e5519f858a6c30f42bfc39a2d4e413ce3f264dbc
Reza Soumi <reza.soumi800@gmail.com>
2023-06-27 20:03:56 +0330

    Simple register, login, send message

--- client.py
+++ client.py
@@ -1,6 +1,8 @@
 import socket
 import threading
 
+user_name = ""
+
 def client_program():
     host = socket.gethostname()  # as both code is running on same pc
     port = 5000  # socket server port number
@@ -10,9 +12,39 @@ def client_program():
     print("Connected to the server.")
 
     def send_message():
+        global user_name
         print("Enter a message (or 'exit' to quit):")
+
         while True:
-            message = input()
+            command = input()
+
+            if command == "exit":
+                client_socket.send("exit".encode())
+                break
+
+            if command == "register":
+                print("Enter username:")
+                username = input()
+                print("Enter password:")
+                password = input()
+                message = "register:" + username + "," + password
+            elif command == "login":
+                print("Enter username:")
+                username = input()
+                print("Enter password:")
+                password = input()
+                message = "login:" + username + "," + password
+            elif command == "message":
+                print("Enter recipient:")
+                receiver = input()
+                print("Enter message:")
+                message = input()
+                content = f"{username},{receiver},{message}"
+                message = "message:" + content
+            else:
+                print("Invalid command. Please try again.")
+                continue
+            
             client_socket.send(message.encode())
 
             if message.lower() == 'exit':
--- server.py
+++ server.py
@@ -1,5 +1,23 @@
 import socket
 import threading
+import json
+
+USERS_FILE = "users.json"  # JSON file to store registered users
+connected_clients = {}
+
+def load_users():
+    try:
+        with open(USERS_FILE, "r") as file:
+            users = json.load(file)
+    except FileNotFoundError:
+        users = {}
+    return users
+
+def save_users(users):
+    with open(USERS_FILE, "w") as file:
+        json.dump(users, file)
+
+users = load_users()
 
 def handle_client(conn, client_address):
     while True:
@@ -9,7 +27,32 @@ def handle_client(conn, client_address):
         
         print("Received from client {}:{}".format(client_address[0], client_address[1]) + " - " + data)
         
-        response = "Server received " + data
+        command, content = data.split(":", 1)
+
+        if command == "register":
+            username, password = content.split(",")
+            if username in users:
+                response = "Username already exists. Please choose a different username."
+            else:
+                users[username] = password
+                save_users(users)
+                response = "Registration successful. You can now login."
+        elif command == "login":
+            username, password = content.split(",")
+            if username in users and users[username] == password:
+                response = "Login successful. Welcome, {}!".format(username)
+                connected_clients[username] = conn
+            else:
+                response = "Invalid username or password. Please try again."
+        elif command == "message":
+            sender, receiver, message = content.split(",", 2)
+            if sender in connected_clients and receiver in connected_clients:
+                receiver_conn = connected_clients[receiver]
+                receiver_conn.send(f"Message from {sender}: {message}".encode())
+                response = "Message sent successfully."
+            else:
+                response = "Sender or receiver is not logged in."
+                
         conn.send(response.encode())
 
     conn.close()
